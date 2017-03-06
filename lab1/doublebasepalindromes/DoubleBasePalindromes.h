208dcf2fcd93928e68af599ab4dae6081add8016
Adrian <adrian.opiela11@gmail.com>
2017-03-06 19:12:34 +0100

    dodanie zadania doublebasepalindromes

---
+++ lab1/doublebasepalindromes/DoubleBasePalindromes.h
@@ -0,0 +1,10 @@
+//
+// Created by adrian on 04.03.17.
+//
+
+#ifndef JIMP_EXERCISES_DOUBLEBASEPALINDROMES_H
+#define JIMP_EXERCISES_DOUBLEBASEPALINDROMES_H
+#include <cstdint>
+uint64_t DoubleBasePalindromes(int max_vaule_exculsive);
+
+#endif //JIMP_EXERCISES_DOUBLEBASEPALINDROMES_H
---
+++ lab1/doublebasepalindromes/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by adrian on 04.03.17.
+//
+#include <iostream>
+#include "DoubleBasePalindromes.h"
+int main()
+{
+    std::cout<<DoubleBasePalindromes(1000000);
+}
