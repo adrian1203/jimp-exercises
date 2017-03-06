43e1fcb6c97b2055320cb3cde89c9fd144cd4759
Adrian <adrian.opiela11@gmail.com>
2017-03-06 19:06:12 +0100

    dodanie zadania

---
+++ lab1/palindrome/Palindrome.cpp
@@ -0,0 +1,26 @@
+//
+// Created by adrian on 04.03.17.
+//
+
+#include "Palindrome.h"
+#include <string>
+bool is_palindrome(std::string str)
+{
+    int wynik=0;
+    size_t dlugosc=str.length();
+    for(int i=0;i<dlugosc;i++)
+    {
+        if(str[i]==str[dlugosc-i-1])
+        {
+            wynik+=1;
+        }
+    }
+    if(wynik==dlugosc)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
---
+++ lab1/palindrome/Palindrome.h
@@ -0,0 +1,11 @@
+//
+// Created by adrian on 04.03.17.
+//
+
+#ifndef JIMP_EXERCISES_PALINDROME_H
+#define JIMP_EXERCISES_PALINDROME_H
+
+#include <string>
+bool is_palindrome(std::string str);
+
+#endif //JIMP_EXERCISES_PALINDROME_H
