d4824cf7b02cfc6bd376bf41855ab6b556cedfee
Adrian <adrian.opiela11@gmail.com>
2017-03-06 20:40:57 +0100

    modyfikacja zadania

--- lab1/palindrome/Palindrome.cpp
+++ lab1/palindrome/Palindrome.cpp
@@ -8,19 +8,9 @@ bool is_palindrome(std::string str)
 {
     int wynik=0;
     size_t dlugosc=str.length();
-    for(int i=0;i<dlugosc;i++)
-    {
-        if(str[i]==str[dlugosc-i-1])
-        {
-            wynik+=1;
-        }
+    for(int i=0;i<dlugosc;i++) {
+        if (str[i] != str[dlugosc - i - 1])
+            return false;
     }
-    if(wynik==dlugosc)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+    return true;
 }
