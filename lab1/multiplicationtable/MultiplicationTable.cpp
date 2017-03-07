afd9c4f449dd2134419f27892095c5603f58c20b
Adrian <adrian.opiela11@gmail.com>
2017-03-07 18:53:45 +0100

    dodanie zadania

--- lab1/multiplicationtable/MultiplicationTable.cpp
+++ lab1/multiplicationtable/MultiplicationTable.cpp
@@ -3,3 +3,27 @@
 //
 
 #include "MultiplicationTable.h"
+#include <iostream>
+void MultiplicationTable(int tab[][10])
+{
+int i,j;
+    for(i=0;i<10;i++)
+    {
+        for (j=0;j<10;j++)
+        {
+            tab[i][j]=(i+1)*(j+1);
+        }
+    }
+}
+void PrintMultiplicationTable(int tab[][10])
+{
+    int i,j;
+    for(i=0;i<10;i++)
+    {
+        for (j=0;j<10;j++)
+        {
+            std::cout<<tab[i][j]<<"\t";
+        }
+        std::cout<<"\n";
+    }
+}
