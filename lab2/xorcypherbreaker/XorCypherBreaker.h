dbd838fee5c203f5e9e58ccd07cb1adce02f15ea
Adrian <adrian.opiela11@gmail.com>
2017-03-14 21:52:36 +0100

    array2d

---
+++ lab2/xorcypherbreaker/XorCypherBreaker.h
@@ -0,0 +1,17 @@
+//
+// Created by adrian on 14.03.17.
+//
+
+#ifndef JIMP_EXERCISES_XORCYPHERBREAKER_H
+#define JIMP_EXERCISES_XORCYPHERBREAKER_H
+#include <iostream>
+#include <map>
+#include <vector>
+#include <math.h>
+#include <string>
+using namespace std;
+std::string XorCypherBreaker(const std::vector<char> &cryptogram,
+                             int key_length,
+                             const std::vector<string> &dictionary);
+
+#endif //JIMP_EXERCISES_XORCYPHERBREAKER_H
