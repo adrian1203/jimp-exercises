5aa855d09e2372deeeb553a7c586c2d07ca3605b
Adrian <adrian.opiela11@gmail.com>
2017-04-10 14:05:29 +0200

    lab6 rozwiaania

--- lab6/counter/WordCounter.h
+++ lab6/counter/WordCounter.h
@@ -12,30 +12,80 @@
 #include <istream>
 #include <fstream>
 #include <vector>
+#include <list>
+#include <set>
+
 using namespace std;
-class Word{
-private:
-    string word_;
-public:
-    Word();
+namespace datastructures {
+    class Word {
+    private:
+        string word_;
+    public:
+        Word();
 
-};
-class Counts{
-private:
-    int value_;
-public:
-    Counts();
+        Word(const string &word);
 
-};
-class WordCounter{
-private:
-    std::pair  <Word,Counts> counter_;
-    vector <string> pr;
-public:
-    WordCounter();
-    WordCounter(const string& name);
+        bool operator==(const Word &other_object) const;
 
-};
+        bool operator<(const Word &other) const;
+
+        bool operator>(const Word &other) const;
+
+        string GetWord(void) const;
+
+        friend class WordCounter;
+    };
+
+    class Counts {
+    private:
+        int value_;
+    public:
+        Counts();
+
+        Counts(int value);
+
+        Counts &operator++(int);
+
+        operator int() const;
+
+        bool operator<(const Counts &other) const;
+
+        bool operator>(const Counts &other) const;
+
+        friend bool operator==(const int int_value, const Counts &other_object);
+
+        int GetCounts(void) const;
+
+    };
+
+    class WordCounter {
+    private:
+        list <pair<Word, Counts>> counter_;
+    public:
+        WordCounter();
+
+        WordCounter(const char *file);
+
+        WordCounter(const initializer_list<Word> list1);
+
+        int DistinctWords();
+
+        int TotalWords();
+
+        Counts operator[](const char *word);
+
+        set<Word> Words(void) const;
+
+        friend ostream &operator<<(std::ostream &is, WordCounter &word_counter);
+
+
+    };
+
+    ostream &operator<<(std::ostream &is, WordCounter &word_counter);
+
+    bool operator==(const int int_value, const Counts &other_object);
+
+}
 
 
 #endif //JIMP_EXERCISES_WORDCOUNTER_H
