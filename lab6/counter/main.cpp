5e175d44f552e5a6e97629e2ec6c962ced2f5ee7
adrian1203 <adrian.opiela11@gmail.com>
2017-04-11 23:24:18 +0200

    lab6 zmiany

--- lab6/counter/main.cpp
+++ lab6/counter/main.cpp
@@ -3,24 +3,20 @@
 //
 #include <iostream>
 #include <fstream>
-
 #include <string>
 #include <iomanip>
 #include <strstream>
 #include <istream>
 
 #include "WordCounter.h"
+
 using namespace std;
-int main(int argc, char **argv){
-    //std::ifstream myfile(argv[1]);
-    //std::string word;
 
-    //if(!myfile)
-        //std::cout << "Nie można otworzyć pliku!";
-    WordCounter w;
-    w.Word1(argv[1]);
-
-
-
+int main(int argc, char **argv) {
+    datastructures::WordCounter w(argv[1]);
+    cout << w;
 }
+
+
+
 
