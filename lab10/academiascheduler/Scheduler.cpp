02d2dcd0e3169eda3dd687e1d38fa77e4ed18c0c
adrian1203 <adrian.opiela11@gmail.com>
2017-05-14 21:32:18 +0200

    c

--- lab10/academiascheduler/Scheduler.cpp
+++ lab10/academiascheduler/Scheduler.cpp
@@ -2,7 +2,7 @@
 // Created by Adrian on 2017-05-13.
 //
 
-#include "Scheduler.h"
+/*#include "Scheduler.h"
 #include <algorithm>
 using namespace std;
 namespace academia{
@@ -15,7 +15,7 @@ namespace academia{
     }
     /*Schedule::Schedule(vector<SchedulingItem> schedule) {
         copy(schedule.begin(), schedule.end(), schedule_);
-    }*/
+    }
     void Schedule::InsertScheduleItem(SchedulingItem v) {
         schedule_.push_back(v);
     }
@@ -30,4 +30,35 @@ namespace academia{
         }
         return tab;
     }
+}*/
+#include "Scheduler.h"
+
+namespace academia{
+
+    Schedule &Schedule::OfTeacher(int teacherid) {
+        Schedule teacher;
+        for (int i = 0; i < schedules.size(); ++i) {
+            if (schedules[i].TeacherId() == teacherid) {
+                teacher.InsertScheduleItem(schedules[i]);
+            }
+        }
+        return this;
+    }
+
+    const Schedule Schedule::OfRoom(int room) {
+        return Schedule();
+    }
+
+    const Schedule Schedule::OfYear(int room) {
+
+        return Schedule();
+    }
+
+    std::vector<int> Schedule::AvailableTimeSlots(int room) {
+        std::vector<int> i;
+
+        return i;
+    }
+
+
 }
--- lab10/academiascheduler/Scheduler.h
+++ lab10/academiascheduler/Scheduler.h
@@ -5,7 +5,7 @@
 #ifndef JIMP_EXERCISES_SCHEDULER_H
 #define JIMP_EXERCISES_SCHEDULER_H
 
-#include <iostream>
+/*#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <array>
@@ -29,5 +29,55 @@ namespace academia{
         int scheduling_[5];
     };
 }
+*/
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <initializer_list>
+#include <list>
+#include <array>
+#include <memory>
 
+namespace academia {
+
+
+    class SchedulingItem {
+    public:
+        SchedulingItem(std::initializer_list<unsigned long> it) {
+            std::copy(it.begin(),it.end(),item.begin());
+        }
+
+        unsigned long CourseId() const { return  item[0];}
+        unsigned long TeacherId() const { return item[1];}
+        unsigned long RoomId() const { return item[2];}
+        unsigned long TimeSlot() const { return item[3];}
+        unsigned long Year() const { return item[4];}
+
+    private:
+        std::vector<unsigned long> item;
+    };
+
+
+    class Schedule {
+    public:
+        Schedule() {}
+
+        void InsertScheduleItem(SchedulingItem k) { schedules.emplace_back(k); }
+
+        Schedule &OfTeacher(int teacherid);
+        const Schedule OfRoom(int room);
+        const Schedule OfYear(int room);
+        std::vector<int> AvailableTimeSlots(int room);
+
+        unsigned long Size() const { return schedules.size(); }
+
+    private:
+        std::vector<SchedulingItem> schedules;
+    };
+
+    SchedulingItem operator[](const Schedule &S, const int &i) {
+        SchedulingItem POINTER = schedules[i];
+        return POINTER;
+    }
+}
 #endif //JIMP_EXERCISES_SCHEDULER_H
