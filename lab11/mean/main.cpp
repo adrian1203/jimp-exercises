d1e038330378548d5a78fe78d11715da346a3c5f
adrian1203 <adrian.opiela11@gmail.com>
2017-05-30 21:30:15 +0200

    q1

--- lab11/mean/main.cpp
+++ lab11/mean/main.cpp
@@ -6,9 +6,10 @@
 #include "Mean.h"
 using namespace std;
 int main(){
-    vector<int> v{1,2,3,4,5,6,7,8,9};
-    cout<<Mean(v);
-    return 0;
+    int t=0;
+    if(!t){
+        cout<<"chuj";
+    }
 
 }
 
