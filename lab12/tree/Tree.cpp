5273d4ff03c8fd53e640b98a36b7aec01017ff74
adrian1203 <adrian.opiela11@gmail.com>
2017-06-01 12:42:21 +0200

    tree

--- lab12/tree/Tree.cpp
+++ lab12/tree/Tree.cpp
@@ -2,4 +2,29 @@
 // Created by Adrian on 2017-05-25.
 //
 
+
 #include "Tree.h"
+namespace tree {
+    //template<class Element>
+
+
+    //template<class Element>
+   /* bool Tree<Element>::operator<(const Element &e) {
+
+        if (this->value < e)return true;
+        else return false;
+
+    }
+*/
+    //template<class Element>
+    /*void Tree<Element>::Insert(const Element &e) {
+        Tree *wsk = this;
+        while (wsk != nullptr) {
+            if (wsk->operator<(e)) wsk = wsk->right;
+            else wsk = wsk->left;
+        }
+        wsk->value = e;
+
+
+    }*/
+}
--- lab12/tree/Tree.h
+++ lab12/tree/Tree.h
@@ -5,26 +5,85 @@
 #ifndef JIMP_EXERCISES_TREE_H
 #define JIMP_EXERCISES_TREE_H
 
-#include <string>
+
+#include <cstdio>
 #include <iostream>
 #include <memory>
+using namespace std;
 namespace tree{
-    template<class T>
-    class Tree;
-    template<class T>
-    class Node{
-        T value;
-        std::unique_ptr<Node<T>> left;
-        std::unique_ptr<Node<T>> right;
 
-    };
-    template<class T>
+
+    template <class Element>
     class Tree{
+
+        shared_ptr<Tree<Element>>  left;
+        shared_ptr<Tree<Element>> right;
+        Element value;
+        size_t size=0;
     public:
+        void Insert(const Element &e);
+        bool Find(const Element &e);
+        size_t Depth();
+        size_t Size();
+        Element Value();
+        Tree(Element v);
+        // size_t Dig(Tree* wsk);
         Tree();
 
-    private:
-        std::unique_ptr<Node<T>> root;
+        bool operator<(const Element &e);
+
+
     };
-}
+
+    template <class Element>
+    Tree<Element>::Tree() {}
+
+    template <class Element>
+    bool Tree<Element>::operator<(const Element &e) {
+
+        if (this->value<e)return true;
+        else return false;
+
+    }
+
+    template <class Element> void Tree<Element>::Insert(const Element &e) {
+        shared_ptr<Tree<Element>> wsk = make_shared<Tree<Element>>(*this);
+        while (wsk != nullptr) {
+            if (wsk->value<e) wsk = wsk->right;
+            else wsk =wsk->left;
+        }
+        wsk->value=e;
+        size++;
+
+
+
+    }
+    template <class Element> Tree<Element>::Tree(Element v) {
+        this->value=v;
+        size++;
+
+    }
+    template <class Element> bool Tree<Element>::Find(const Element &e){}
+    template <class Element>  size_t Tree<Element>::Depth(){
+        return 1;
+
+    }
+    template <class Element>  size_t Tree<Element>::Size(){
+
+        return size;
+    }
+    template <class Element> Element Tree<Element>:: Value(){
+        return value;
+    }
+
+    /*template <class Element> size_t Tree<Element>::Dig(Tree *wsk) {
+        if (wsk!=NULL){
+            Dig(wsk->left);
+            Dig(wsk->right);
+        }
+    }*/
+
+
+};
 #endif //JIMP_EXERCISES_TREE_H
+
