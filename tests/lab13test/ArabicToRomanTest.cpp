0cfab010eb144ae8fcf0c14aafcd9118b21f4b3b
adrian1203 <adrian.opiela11@gmail.com>
2017-06-03 13:00:04 +0200

    testy 13 lab

--- tests/lab13test/ArabicToRomanTest.cpp
+++ tests/lab13test/ArabicToRomanTest.cpp
@@ -1,4 +1,33 @@
 //
 // Created by mwypych on 01.06.17.
 //
+#include <gtest/gtest.h>
+#include "../../lab13/arabicroman/ArabicToRoman.h"
 
+using namespace arabicroman;
+
+class ArabicRomanTest: public ::testing::Test{
+public:
+};
+TEST_F(ArabicRomanTest, DoConvertToERROR){
+    EXPECT_EQ("", ConversionToRoman(4252));
+}
+TEST_F(ArabicRomanTest, DoConvertToERROR1){
+    EXPECT_EQ("", ConversionToRoman(-2));
+}
+TEST_F(ArabicRomanTest, DoConvertToI){
+    EXPECT_EQ("I", ConversionToRoman(1));
+}
+
+TEST_F(ArabicRomanTest, DoConvertToC){
+    EXPECT_EQ("C", ConversionToRoman(100));
+}
+TEST_F(ArabicRomanTest, DoConvertToXXX){
+    EXPECT_EQ("XXX", ConversionToRoman(30));
+}
+TEST_F(ArabicRomanTest, DoConvertToMMMCMXCIX){
+    EXPECT_EQ("MMMCMXCIX", ConversionToRoman(3999));
+}
+TEST_F(ArabicRomanTest, DoConvertToDCCCLXXV){
+    EXPECT_EQ("DCCCLXXV", ConversionToRoman(875));
+}
--- tests/lab13test/RomanToArabicTest.cpp
+++ tests/lab13test/RomanToArabicTest.cpp
@@ -1,4 +1,29 @@
 //
 // Created by mwypych on 01.06.17.
 //
+#include <gtest/gtest.h>
+#include "../../lab13/arabicroman/ArabicToRoman.h"
 
+using namespace arabicroman;
+
+class RomanArabicTest: public ::testing::Test{
+public:
+};
+TEST_F(RomanArabicTest, DoConvertTo){
+    EXPECT_EQ("", ConversionToArabic(""));
+}
+TEST_F(RomanArabicTest, DoConvertTo1){
+    EXPECT_EQ("1", ConversionToArabic("I"));
+}
+TEST_F(RomanArabicTest, DoConvertTo23){
+    EXPECT_EQ("23", ConversionToArabic("XXIII"));
+}
+TEST_F(RomanArabicTest, DoConvertTo256){
+    EXPECT_EQ("256", ConversionToArabic("CCLVI"));
+}
+TEST_F(RomanArabicTest, DoConvertTo3589){
+    EXPECT_EQ("3589", ConversionToArabic("MMMDLXXXIX"));
+}
+TEST_F(RomanArabicTest, DoConvertToERROR){
+    EXPECT_EQ("ERROR", ConversionToArabic("MMMMDLXXXIX"));
+}
