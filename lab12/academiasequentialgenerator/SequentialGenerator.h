fdd4c266aa2bf418becdf12c71198d28e9d7c315
adrian1203 <adrian.opiela11@gmail.com>
2017-06-05 21:36:14 +0200

    sequentialgenerator

--- lab12/academiasequentialgenerator/SequentialGenerator.h
+++ lab12/academiasequentialgenerator/SequentialGenerator.h
@@ -5,4 +5,27 @@
 #ifndef JIMP_EXERCISES_SEQUENTIALGENERATOR_H
 #define JIMP_EXERCISES_SEQUENTIALGENERATOR_H
 
+
+template <class T,class Y>
+class  SequentialIdGenerator{
+    Y value;
+public:
+    SequentialIdGenerator(Y iter);
+    SequentialIdGenerator();
+    int NextValue();
+};
+template <class T,class Y> SequentialIdGenerator<T,Y>::SequentialIdGenerator(Y iter) {
+    //value= static_cast<int>(iter)-1;
+    value=iter-1;
+
+}
+template <class T,class Y> SequentialIdGenerator<T,Y>::SequentialIdGenerator(){
+    value=-1;
+};
+template <class T,class Y> int SequentialIdGenerator<T,Y>::NextValue() {
+    int x;
+    ++value;
+    return value;
+
+}
 #endif //JIMP_EXERCISES_SEQUENTIALGENERATOR_H
